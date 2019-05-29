95d7eaa36696a786e1c46d9db8492650f7bf7082
folinhuakai <1242014273@qq.com>
2019-05-29 14:00:13 +0800

    向量与矩阵相乘，不修改w值

--- ConsoleApplication1/Matrix.h
+++ ConsoleApplication1/Matrix.h
@@ -444,7 +444,7 @@ namespace maki {
 		tmp.x = v.x * m[0][0] + v.y * m[1][0] + v.z* m[2][0] + v.w*m[3][0];
 		tmp.y = v.x * m[0][1] + v.y * m[1][1] + v.z* m[2][1] + v.w*m[3][1];
 		tmp.z = v.x * m[0][2] + v.y * m[1][2] + v.z* m[2][2] + v.w*m[3][2];
-		tmp.w = v.x * m[0][3] + v.y * m[1][3] + v.z* m[2][3] + v.w*m[3][3];
+		--tmp.w = v.x * m[0][3] + v.y * m[1][3] + v.z* m[2][3] + v.w*m[3][3];
 		return tmp;
 	}
 
